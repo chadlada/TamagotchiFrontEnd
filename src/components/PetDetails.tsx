cf11d7520b3b66393d4c116d3afcc315c6706918
chadlada <chadlada@gmail.com>
2022-05-04 22:52:42 -0400

    Added buttons, Loads details for each pet. Working on making buttons work with api and loading new state accordingly

--- src/components/PetDetails.tsx
+++ src/components/PetDetails.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 import { PetType } from '../App'
+// import image from './images/playwithme.png'
 
 export function PetDetails() {
   const [petDetails, setPetDetails] = useState<PetType>({
@@ -11,10 +14,56 @@ export function PetDetails() {
     lastInteractedWithDate: '',
     isDead: false,
   })
+  const params = useParams<{
+    id: string
+    actions: 'Playtimes' | 'Feedings' | 'Scoldings'
+  }>()
+
+  const [petPlay, setPetPlay] = useState<number>()
+  const [petHunger, setPetHunger] = useState<number>()
+
+  useEffect(() => {
+    async function fetchPetDetails() {
+      const response = await axios.get(
+        `https://chadladatamagotchi.herokuapp.com/api/pets/${params.id}`
+      )
+      if (response.status === 200) {
+        setPetDetails(response.data)
+        // console.log(response.data)
+      }
+    }
+    fetchPetDetails()
+  }, [params.id])
+
+  async function feedPet() {
+    const response = await axios.post(
+      `https://chadladatamagotchi.herokuapp.com/api/pets/${params.id}/Feedings`
+    )
+    if (response.status === 200) {
+      setPetHunger(response.data)
+    }
+  }
 
   return (
-    <div>
-      <p>Pet Details Page</p>
-    </div>
+    <>
+      <p className="detailsp">Interact with your pet!</p>
+      <div className="interact">
+        <button>Play</button>
+        <button>Pet</button>
+        <button onClick={feedPet}>Feed {petHunger}</button>
+        <button>Scold</button>
+        <br></br>
+        <br />
+        <button>Delete </button>
+      </div>
+      <h2>~Pet Details~</h2>
+      <div className="pet-details">
+        <p>Pet ID: {params.id}</p>
+        <p>Name: {petDetails.name}</p>
+        <p>Birthday: {petDetails.birthday}</p>
+        <p>Hunger: {petDetails.hungerLevel}</p>
+        <p>Happiness: {petDetails.happinessLevel}</p>
+      </div>
+    </>
   )
 }
